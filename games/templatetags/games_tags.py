8ce134370f382acb61f3a7cf4cf120cd455e7e7f
Sean Dodsworth <sean@dodsworth.org>
2023-12-03 07:56:33 +0000

    Update pagination

--- games/templatetags/games_tags.py
+++ games/templatetags/games_tags.py
@@ -31,45 +31,53 @@ def snippet(name):
 
 
 @register.inclusion_tag('games/_pagination.html', takes_context=True)
-def pagination(context, number_adjacent=1):
+def pagination(context, max_pages=10):
 
     args = context.get('args', '')
-
     pages = []
     current_page_number = context['page_obj'].number
     paginator = context['page_obj'].paginator
-    last_page_visible = True
 
     for i in paginator.page_range:
-        show_page = False
         classes = ['pagination-link']
+        distance_from_current = abs(current_page_number - i)
 
-        if current_page_number == i:
+        is_current = current_page_number == i
+        if is_current:
             classes.append('is-current')
-            show_page = True
-        elif abs(current_page_number - i) <= number_adjacent:
-            classes.append('is-adjacent')
-            show_page = True
-        if i == 1:
-            classes.append('is-first')
-            show_page = True
-        if i == paginator.num_pages:
-            classes.append('is-last')
-            show_page = True
 
-        if show_page:
-            page = {
-                'text': str(i),
-                'class': ' '.join(classes),
-                'href': f"?{args}&page={i}",
-                'visible': show_page,
-            }
-            pages.append(page)
-            last_page_visible = True
-        elif last_page_visible:
-            page = {'visible': False}
-            pages.append(page)
-            last_page_visible = False
+        page = {
+            'text': str(i),
+            'class': ' '.join(classes),
+            'href': f"?{args}&page={i}",
+            'first': i == 1,
+            'last': i == paginator.num_pages,
+            'current': is_current,
+            'order': i,
+            'distance_from_current': distance_from_current,
+        }
+
+        pages.append(page)
+
+    if len(pages) > max_pages:
+        min_pages = [x for x in pages if x['current'] or x['first'] or x['last']]
+        min_page_orders = [x['order'] for x in min_pages]
+        num_extra_pages_required = max_pages - len(min_pages)
+        available_pages = [x for x in pages if x['order'] not in min_page_orders]
+        extra_pages = sorted(available_pages, key=lambda x: x['distance_from_current'])[:num_extra_pages_required]
+        raw_pages = min_pages + extra_pages
+        raw_pages.sort(key=lambda x: x['order'])
+
+        pages = []
+        for i in range(0, len(raw_pages)):
+            this_page = raw_pages[i]
+            pages.append(this_page)
+            try:
+                next_page = raw_pages[i + 1]
+                if next_page['order'] - this_page['order'] > 1:
+                    pages.append({})
+            except IndexError:
+                break
 
     return {
         'pages': pages,
